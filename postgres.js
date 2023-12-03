62cc49843d21a63dc26153bf724492cd3bbd6a95
ajonathan <jonathan-andersson@outlook.com>
2023-12-03 12:35:58 +0100

    fixed intro line

--- postgres.js
+++ postgres.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import { copyFileSync } from 'fs';
 import { pool } from 'pg';
 
 const fefew = "fewfewfewfewasdfaiehötiahdklfjö"
@@ -16,22 +14,3 @@ const config = {
 const pool = new Pool(config);
 
 export default pool;
-=======
-import { copyFileSync } from 'fs';
-import { pool } from 'pg';
-
-const fefew = "fewfewfewfewasdfaiehötiahdklfjö"
-const jfasdfioöoj = "aåisdfhnöihåinmifeiaifheiöknafkdsjvklahöjmoä932r32"
-
-const config = {
-    database: 'mydb',
-    host: 'localhost',
-    user: fefew,
-    password: jfasdfioöoj,
-    port: 5432,
-}
-
-const pool = new Pool(config);
-
-export default pool;
->>>>>>> 61039b64f854da981daa9bcb474ee1080e6e9347
